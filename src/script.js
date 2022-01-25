7873c876eb39c1a1ce6bf54c4b8030dca4a5920b
Jiahua Cui <jiahua.cui@dcmail.ca>
2022-01-25 13:13:30 -0500

    added landing animation and work div image animation

--- src/script.js
+++ src/script.js
@@ -1,4 +1,11 @@
 const caseScroll = document.querySelector(".case-text-container");
+const tapHand = document.getElementById("tap-hand");
+const phoneHand = document.getElementById("phone-hand");
+const bird = document.getElementById("bird");
+const heart = document.getElementById("heart");
+const head = document.getElementById("head");
+const landingContainer = document.querySelector(".landing-hero");
+const workArray = document.querySelectorAll(".work");
 // const loadScreen = document.querySelector(".loading");
 
 // loadTimeline = gsap.timeline();
@@ -24,7 +31,87 @@ const caseScroll = document.querySelector(".case-text-container");
 //   },
 // });
 
-//case study scroll left right on window scroll
+//hero img animation
+const initAnim = () => {
+  //check if on design or dev
+  if (tapHand) {
+    gsap.to(bird, {
+      rotate: 15,
+      duration: 1,
+      ease: "Linear,easeNone",
+      yoyo: true,
+      repeat: -1,
+    });
+    designAnim = gsap.timeline();
+    designAnim.to(tapHand, {
+      x: -130,
+      y: -100,
+      duration: 1,
+    });
+    designAnim.to(tapHand, {
+      rotation: -10,
+      duration: 0.5,
+      yoyo: true,
+      repeat: 1,
+    });
+    designAnim.to(tapHand, {
+      x: 0,
+      y: 0,
+      duration: 1,
+    });
+    designAnim.to(head, {
+      scale: 0.8,
+      duration: 2.5,
+      ease: "Elastic.easeOut",
+      delay: -0.5,
+    });
+  }
+};
+
+initAnim();
+
+const heroClickAnim = () => {
+  if (tapHand) {
+    heartAnim = gsap.timeline();
+    heartAnim.fromTo(
+      heart,
+      {
+        scale: 0,
+        x: 0,
+        y: 0,
+      },
+      {
+        scale: 0.5,
+        x: 120,
+        y: Math.floor(Math.random() * 200),
+        duration: 0.2,
+      }
+    );
+  }
+};
+
+landingContainer.onclick = heroClickAnim;
+
+workArray.forEach((work) => {
+  work.onmouseenter = function () {
+    imgCon = work.querySelector(".image-container");
+    gsap.to(imgCon, {
+      height: "20rem",
+      duration: 1,
+      ease: "Expo.easeOut",
+    });
+  };
+  work.onmouseleave = function () {
+    imgCon = work.querySelector(".image-container");
+    gsap.to(imgCon, {
+      height: "0rem",
+      duration: 1,
+      ease: "Expo.easeOut",
+    });
+  };
+});
+
+//case study side scroll on window scroll
 let scrollOldValue = 0;
 let scrollNewValue = 0;
 let scrollUp = false;
